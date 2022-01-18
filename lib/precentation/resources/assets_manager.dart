5486bd431ad8c5be6babff16593abaab3f934db8
bazl <you@example.com>
2022-01-19 00:38:55 +0530

    finished noted

--- lib/precentation/resources/assets_manager.dart
+++ lib/precentation/resources/assets_manager.dart
@@ -2,4 +2,8 @@ const String image_Path = 'assets/images';
 
 class ImageAssets {
   static const String splashLogo = '$image_Path/splash_logo.png';
+  static const String onboardingLogo1 = '$image_Path/onboarding_logo1.svg';
+  static const String onboardingLogo2 = '$image_Path/onboarding_logo2.svg';
+  static const String onboardingLogo3 = '$image_Path/onboarding_logo3.svg';
+  static const String onboardingLogo4 = '$image_Path/onboarding_logo4.svg';
 }
